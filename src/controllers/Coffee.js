6f3df73c8167ae6d0add0a7547b3c9d3f42f5cec
Alexandra Romanova <alexandraromanova@Alexandras-Air.home>
2019-06-08 13:26:47 -0400

    create route

--- src/controllers/Coffee.js
+++ src/controllers/Coffee.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Reviews from './Reviews.js'
+//import Reviews from './Reviews.js'
 import Form from './FormUpdateCoffee.js';
+import FormNew from './FormNew.js';
 
 class Coffee extends Component {
     constructor(props) {
@@ -9,9 +10,14 @@ class Coffee extends Component {
             coffees: [],
            isLoaded: false
         }
+        this.handleAdd = this.handleAdd.bind(this)
+        this.handleCreateChange = this.handleCreateChange.bind(this)
         this.handleChange = this.handleChange.bind(this)
         this.handleUpdate = this.handleUpdate.bind(this)
+       //this.handleDelete = this.handleDelete.bind(this)
     }
+
+//*****GET all coffees **********/
     componentDidMount() {
         this.getCoffees()
     }
@@ -23,6 +29,53 @@ class Coffee extends Component {
             .catch(error => console.error(error))
     }
 
+//*******CREATE******** 
+handleCreateChange(event) {
+    this.setState({
+        [event.target.id]: event.target.value
+    },
+    )}
+
+handleAdd (event) {
+   event.preventDefault()
+   console.log(event.target)
+   let formInputs = { 
+    name: this.state.name,
+    origin: this.state.origin,
+    notes: this.state.notes,
+    img: this.state.img,
+    price: this.state.price 
+   }
+    fetch('/coffees/', {
+      body: JSON.stringify(formInputs),
+      method: 'POST',
+   headers: {
+     'Accept': 'application/json, text/plain, */*',
+     'Content-Type': 'application/json'
+   }
+  })
+   .then(createdCoffee => {
+     return createdCoffee.json()
+   })
+   .then(jsonedCoffee => {
+     // reset the form
+     // add coffee to coffees
+     this.setState({
+       formInputs: {
+        name: '',
+        origin: '',
+        notes: '',
+        img: '',
+        price: ''
+       },
+       coffees: [jsonedCoffee, ...this.state.coffees]
+     })
+   })
+   .catch(error => console.log(error))
+  }
+  //... and pass this into the FormNew
+
+//UPDATE *****************
     handleChange (event) {
         //track changes to input (this will be a "handleChange") this will have the data (stored in state)
         this.setState({
@@ -30,8 +83,7 @@ class Coffee extends Component {
             
         )
       }
-
-      
+ 
     handleUpdate (event) {
         //submit the changes (this will be a "handlesubmit, handleadd") this will take the data (stored in state) and PUT/PATCH to backend
         event.preventDefault()
@@ -54,7 +106,29 @@ class Coffee extends Component {
        })
        .catch(error => console.log(error))
       }
+//**********DELETE */
+// handleDelete (deletedCoffee) {
 
+//     fetch(`/coffees/${deletedCoffee.id}`, {
+//       method: 'DELETE',
+//    headers: {
+//      'Accept': 'application/json, text/plain, */*',
+//      'Content-Type': 'application/json'
+//    }
+//   })
+//    .then(json => {
+//     this.setState(state => {
+//         const coffees = state.coffees.filter((coffee, index) => coffee.id !== deletedCoffee.id)
+//         return {
+//           coffees,
+//         }
+//       }) 
+    
+//    })
+//    .catch(error => console.log(error))
+//   }
+
+//********************************** */
     render() {
         console.log(this.state)
         //console.log(coffees.reviews)
@@ -65,13 +139,17 @@ class Coffee extends Component {
             <span role="img" aria-label="sheep">☕</span>
                Welcome to Coffee Time !</h1>
                 </header>
-                <div className='container'>
+    
+                <div className='container'>   
+<section className='sidebar'> 
+<h3>Add Coffee here</h3>
+<FormNew create={this.handleAdd} change={this.handleCreateChange}/>
+</section>
                 {
                     this.state.coffees.map(coffee => {
                         return (
                             <div className='card'  key={coffee.id}>
-
-                                
+ 
                                 <h3 className="card-header">Blend Name: {coffee.name}</h3>
                                 <div className="card-body">
                                 <h3>Origin: {coffee.origin}</h3>
@@ -85,15 +163,16 @@ class Coffee extends Component {
 <Form update = {this.handleUpdate} change={this.handleChange} id = {coffee.id}
  name={coffee.name} img = {coffee.img} price = {coffee.price}/>
 }
+{/* <button onClick={()=> handleDelete(coffee)}> X </button>  */}
+
 </div>
   {/* <Reviews reviews={coffee.reviews}/> */}
-                             </div>
-                             
-                            
+                             </div>    
                         )
                     }) 
                 }
                 </div>
+
             </>
         )
     }
---
+++ src/controllers/FormNew.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+
+class FormNew extends Component {
+
+   //******************************* */
+   render() {
+      console.log(this.props.create)
+      return (
+         <>
+            <form  onSubmit={this.props.create} >
+               <div className="form-group">
+                  <label for="exampleInputTitle">Coffee name</label>
+                  <input className="form-control"
+                     onChange={this.props.change}
+                     name={'name'}
+                     placeholder={'name'}
+                     type={'text'}
+                     defaultValue={this.props.name}
+                     id={'name'}
+                  />
+               </div>
+
+               <div className="form-group">
+                  <label for="exampleInputImage">Image </label>
+                  <input className="form-control"
+                     onChange={this.props.change}
+                     name={'img'}
+                     placeholder={'img'}
+                     type={'text'}
+                     defaultValue={this.props.img}
+                     id={'img'}
+                  />
+               </div>
+
+               <div className="form-group">
+                  <label for="exampleInputDescription">Notes</label>
+                  <input className="form-control"
+                     onChange={this.props.change}
+                     name={'notes'}
+                     placeholder={'notes'}
+                     type={'text'}
+                     defaultValue={this.props.notes}
+                     id={'notes'}
+                  />
+               </div>
+
+               <div className="form-group">
+                  <label for="exampleInputSize">Price </label>
+                  <input className="form-control"
+                     onChange={this.props.change}
+                     name={'price'}
+                     placeholder={'price'}
+                     type={'text'}
+                     defaultValue={this.props.price}
+                     id={'price'}
+                  />
+               </div>
+
+               <div className="form-group">
+                  <label for="exampleInputStyle">Origin</label>
+                  <input className="form-control"
+                     onChange={this.props.change}
+                     name={'origin'}
+                     placeholder={'origin'}
+                     type={'text'}
+                     defaultValue={this.props.origin}
+                     id={'origin'}
+                  />
+               </div>
+
+               {/* onClick if this.props.update(Art.js) is false, than run this.props.create(User_id.js) */}
+              <input type='submit' value='submit' onClick={this.props.create} />
+            </form>
+         </>
+      )
+   }
+}
+export default FormNew
